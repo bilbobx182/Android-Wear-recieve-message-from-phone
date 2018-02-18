349262053f784df74120b9e2edaae5e5dc9f485b
bilbobx182 <c14474048@mydit.ie>
2018-02-18 10:51:01 +0000

    cleaning up code

--- wear/src/main/java/com/github/bilbobx182/phonewatchcommunication/MainActivity.java
+++ wear/src/main/java/com/github/bilbobx182/phonewatchcommunication/MainActivity.java
@@ -1,37 +1,22 @@
 package com.github.bilbobx182.phonewatchcommunication;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.wearable.activity.WearableActivity;
 import android.util.Log;
 import android.widget.TextView;
 
-import com.google.android.gms.wearable.DataClient;
-import com.google.android.gms.wearable.DataEventBuffer;
 import com.google.android.gms.wearable.MessageClient;
 import com.google.android.gms.wearable.MessageEvent;
 import com.google.android.gms.wearable.Wearable;
 
-public class MainActivity extends WearableActivity implements DataClient.OnDataChangedListener, MessageClient.OnMessageReceivedListener{
+public class MainActivity extends WearableActivity implements  MessageClient.OnMessageReceivedListener{
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Wearable.getDataClient(this).addListener(this);
         Wearable.getMessageClient(this).addListener(this);
-    }
-
-    @Override
-    public void onDataChanged(@NonNull DataEventBuffer dataEventBuffer) {
-        Log.d("MainActivity","MessageRecieved");
-        updateText();
-    }
-
-    private void updateText() {
-        TextView text = findViewById(R.id.myTextView);
-        text.setText("IT WORKED!");
     }
 
     @Override
@@ -40,20 +25,17 @@ public class MainActivity extends WearableActivity implements DataClient.OnDataC
         TextView text = findViewById(R.id.myTextView);
         String income = new String(messageEvent.getData());
         text.setText(income);
-
-
-        // Check to see if the message is to start an activity
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        Wearable.getDataClient(this).addListener(this);
+        Wearable.getMessageClient(this).addListener(this);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Wearable.getDataClient(this).removeListener(this);
+        Wearable.getMessageClient(this).removeListener(this);
     }
 }
