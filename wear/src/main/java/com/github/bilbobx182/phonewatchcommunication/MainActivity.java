17859cb0152f8ca8b06cc1fa5c2f94727031a1c1
bilbobx182 <c14474048@mydit.ie>
2018-02-18 10:54:57 +0000

    final cleanup

--- wear/src/main/java/com/github/bilbobx182/phonewatchcommunication/MainActivity.java
+++ wear/src/main/java/com/github/bilbobx182/phonewatchcommunication/MainActivity.java
@@ -2,7 +2,6 @@ package com.github.bilbobx182.phonewatchcommunication;
 
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
-import android.util.Log;
 import android.widget.TextView;
 
 import com.google.android.gms.wearable.MessageClient;
@@ -11,18 +10,18 @@ import com.google.android.gms.wearable.Wearable;
 
 public class MainActivity extends WearableActivity implements  MessageClient.OnMessageReceivedListener{
 
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = findViewById(R.id.myTextView);
         Wearable.getMessageClient(this).addListener(this);
     }
 
     @Override
     public void onMessageReceived(MessageEvent messageEvent) {
-        Log.d("MAINACTIVITY","RECIEVED");
-        TextView text = findViewById(R.id.myTextView);
         String income = new String(messageEvent.getData());
         text.setText(income);
     }
